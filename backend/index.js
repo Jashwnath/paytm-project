82ecb022e8271d190dda0a0ea58848fdcc83546d
Jashwanth Alwaru <alwarujashwanth@gmail.com>
2026-02-23 00:05:42 +0530

    Converted to ES module syntax

--- backend/index.js
+++ backend/index.js
@@ -2,8 +2,8 @@
 import express, { json } from "express";
 import cors from "cors";
 import rootRouter from "./routes/index.js";
-require("dotenv").config();
-
+import dotenv from "dotenv";
+dotenv.config();
 const app = express();
 
 app.use(cors());
